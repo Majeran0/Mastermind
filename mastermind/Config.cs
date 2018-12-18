b56f3eef19d02fb523676c9f1eb70251bc0ac84a
tropok <tropok@gmail.com>
2018-12-18 22:29:06 +0100

    Ostatni na dziś

--- mastermind/Config.cs
+++ mastermind/Config.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace mastermind {
-	class Config {
+	public sealed class Config {
 		public int MaxTurns { get; set; } = 12;
 		public int MaxColors { get; set; } = 4;
 	}
